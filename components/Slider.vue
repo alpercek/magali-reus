801b1dfcef1d662df97f5b5a10aa08e5a727d710
John Doe <alpercekinmez7@gmail.com>
2023-10-07 01:26:34 -0700

    show on hover

--- components/Slider.vue
+++ components/Slider.vue
@@ -36,7 +36,7 @@
   <div
         v-if="swiperOptions.navigation"
         :key="controlsClass"
-        :class="`custom-controls-container ${controlsClass}`"
+        :class="`custom-controls-container show-on-hover ${controlsClass}`"
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="15.751" height="28.675" viewBox="0 0 15.751 28.675" :class="`${controlsClass}-previous previous w-12 h-12`">
           <path
