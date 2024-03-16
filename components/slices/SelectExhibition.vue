434e3f9e9a3120af1150e4aab7b933473e95071f
John Doe <alpercekinmez7@gmail.com>
2024-03-16 10:25:58 -0700

    share button update

--- components/slices/SelectExhibition.vue
+++ components/slices/SelectExhibition.vue
@@ -75,8 +75,13 @@
           <div class="cursor-pointer select-none" @click="closePreview">
             ✕
           </div>
-          <div class="cursor-pointer select-none" @click="copyShare">
-            SHARE
+          <div class="cursor-pointer select-none fill-magali" @click="copyShare">
+            <svg xmlns="http://www.w3.org/2000/svg"
+            height="48"
+            viewBox="0 0 24 24"
+            width="48"
+            focusable="false"
+            style="pointer-events: none;"><path d="M15 5.63 20.66 12 15 18.37V14h-1c-3.96 0-7.14 1-9.75 3.09 1.84-4.07 5.11-6.4 9.89-7.1l.86-.13V5.63M14 3v6C6.22 10.13 3.11 15.33 2 21c2.78-3.97 6.44-6 12-6v6l8-9-8-9z"></path></svg>
           </div>
         </div>
         <ImageColumn :images="work.data.body.filter(slice => slice.slice_type === 'image')" class="pt-1" />
